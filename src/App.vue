0bcbefcd5f31248e86233f5e370481bc45c33050
Stefan Urbansky <urbansky@hxcode.io>
2022-01-11 09:48:30 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import PropertyBox from './components/PropertyBox.vue'
+
+const flexDirection = {
+  title: "Direction",
+  items: [
+    { tailwind: "flex-row", css: "flex-direction: row" },
+    { tailwind: "flex-row-reverse", css: "flex-direction: row-reverse" },
+    { tailwind: "flex-col", css: "flex-direction: column" },
+    { tailwind: "flex-col-reverse", css: "flex-direction: column-reverse" },
+  ]
+}
+
+</script>
+
+<template>
+  <div class="container mx-auto mt-14">
+    <h1 class="text-2xl font-bold">
+      Tailwind v3 Flex Properties
+    </h1>
+    <div class="grid grid-cols-2 gap-x-24 mt-10">
+      <div>
+        <h2 class="font-bold">Container</h2>
+<!--        <property-box property="display" type="container" -->
+<!--            :items="['flex']"-->
+<!--        >-->
+<!--        </property-box>-->
+        <property-box :data="flexDirection" type="container" />
+        <property-box :data="flexDirection" type="container" />
+<!--        <property-box property="flex-wrap" type="container"-->
+<!--            :items="['flex-wrap','flex-wrap-reverse','flex-nowrap']">-->
+<!--        </property-box>-->
+      </div>
+      <div>
+        <h2 class="font-bold">Items</h2>
+<!--        <property-box property="order" type="items"-->
+<!--            :items="['order1 - order12','order-none','order-first','order-last']">-->
+<!--        </property-box>-->
+<!--        <property-box property="flex-grow" type="items"-->
+<!--            :items="['grow','grow-0']">-->
+<!--        </property-box>-->
+<!--        <property-box property="flex-shrink" type="items"-->
+<!--            :items="['shrink','shrink-0']">-->
+<!--        </property-box>-->
+      </div>
+    </div>
+    <div class="mt-20">
+      Inspired by an <a target="blank" href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">article from CSS-TRICKS</a>
+    </div>
+  </div>
+</template>
+
+
+<style>
+a {
+  @apply text-blue-400 hover:underline
+}
+.tailwindColor {
+  color: #0EA5E9;
+}
+.cssColor {
+  color: #E546AF;
+}
+</style>
---
+++ src/components/PropertyBox.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+  import {ref} from "vue";
+
+  defineProps<{
+    type: "container" | "items",
+    data: object
+  }>()
+  const containerClass = ref("flex-row");
+  function testStart() {
+    containerClass.value = "flex-row-reverse";
+  }
+  function testEnd() {
+    containerClass.value = "flex-row";
+  }
+</script>
+
+<template>
+  <div class="property-box" :class="{ 'flex-container' : type === 'container', 'flex-items': type === 'items'}">
+    <div class="title">{{ data.title }}</div>
+    <div class="showbox" :class="containerClass">
+      <div class="item"><span>1</span></div>
+      <div class="item"><span>2</span></div>
+      <div class="item"><span>3</span></div>
+    </div>
+    <ul class="tailwind-list">
+      <li class="flex header">
+        <div class="w-1/2">Tailwind Class</div>
+        <div class="w-1/2">CSS Property</div>
+      </li>
+      <li v-for="item in data.items" :key="item" class="flex data" @mouseenter="testStart" @mouseleave="testEnd">
+        <div class="w-1/2 tailwindColor">{{ item.tailwind }}</div>
+        <div class="w-1/2 cssColor">{{ item.css }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+  .property-box {
+    @apply my-8 px-8 py-6 rounded-lg;
+  }
+  .flex-container {
+    background: linear-gradient(180deg, rgba(189, 104, 255, 0.17) 0%, rgba(196, 196, 196, 0) 100%);
+  }
+  .flex-items {
+    background: linear-gradient(180deg, rgba(255, 167, 104, 0.33) 0%, rgba(196, 196, 196, 0) 100%);
+  }
+  .property-box .title {
+    @apply mb-4 text-lg font-bold text-black;
+  }
+
+  .showbox {
+    @apply flex bg-white gap-4 rounded-lg mb-4;
+    height: 80px;
+    padding: 20px;
+  }
+  .showbox .item {
+    @apply h-full bg-green-500 rounded shadow flex justify-center items-center;
+    width: 40px;
+  }
+  .showbox .item > span {
+    @apply text-white;
+  }
+
+  .tailwind-list {
+    /*@apply ml-8*/
+  }
+  .tailwind-list li {
+    @apply border-b border-gray-300 py-1
+  }
+  .tailwind-list li:hover {
+    @apply bg-gray-100;
+  }
+  .tailwind-list li.header > div {
+    @apply text-gray-400 text-xs
+  }
+  .tailwind-list li.data > div {
+    /*@apply text-lg*/
+  }
+
+</style>
